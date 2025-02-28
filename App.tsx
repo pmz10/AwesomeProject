4a6e4df201a446c6e8b811a5a464068500906b9e
pmz10 <welcome_to_pmz@hotmail.com>
2025-02-27 21:17:30 -0600

    Cambios y carga componentes Native Paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SafeAreaView } from 'react-native';
 import { HelloScreen } from './src/Presentation/Screens/HelloScreen';
 import CounterScreen from './src/Presentation/Screens/CounterScreen';
+import { PaperProvider } from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/*<HelloScreen name = 'Gerardo García' />*/}
-      <CounterScreen />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          {/*<HelloScreen name = 'Gerardo García' />*/}
+          <CounterScreen />
+        </SafeAreaView>
+      </PaperProvider>
+    </SafeAreaProvider>
   )
 }
